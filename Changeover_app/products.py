714492b6b1eed966953af2a2e8289508199e007e
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-04-16 20:44:55 +0200

    Building the product class so it can ingest data from csv, convert to multiindex and locate products

--- Changeover_app/products.py
+++ Changeover_app/products.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
-class Product:
-    _PRODUCTS = {'Prod_1' : 240, 'Prod_2': 45, 'Prod_3' : 30}
-
+class Product():
+    _DF = pd.read_csv('data/matrix.csv', index_col=[0], header=[0, 1, 2])
+    
     def __init__(self, prod1, prod2):
         self.prod1 = prod1
         self.prod2 = prod2
     
+    def show_matrix(self):
+        return self._DF.head()
+
     def locate_keys(self):
-        return list(_PRODUCTS.keys())
+        return list(self._DF.index)
     
-    def locate_products(self):
-        return _PRODUCTS.xs(self.prod1, axis=0, level=None)[self.prod2].to_dict()
+    def locate_product(self):
+        return self._DF.xs(self.prod1, axis=0, level=None)[self.prod2].to_dict()
 
 
-product = Product('Prod_1', 'Prod_2')
-product.locate_keys()
-    
-    
-    
+product1 = Product('Prod_1', 'Prod_3')
+product2 = Product('Prod_4', 'Prod_20')
