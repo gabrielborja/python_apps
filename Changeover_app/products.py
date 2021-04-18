83c3e1551e5bd56e1844d892fed90d186ffc4324
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-04-18 19:31:53 +0200

    Update docs, string representation and list product method

--- Changeover_app/products.py
+++ Changeover_app/products.py
@@ -1,18 +1,35 @@
 import pandas as pd
 
 class Product():
+    """Convert from one manufacturing product to a second product from available list.
+    Return the product id code, group category and time in minutes needed to change
+    from one product to another (changeover time).
+    
+    Parameters
+    ----------
+    prod1 = str of first product name from data/matrix.csv
+    prod2 = str of second product name from data/matrix.csv"""
+
     _DF = pd.read_csv('data/matrix.csv', index_col=[0], header=[0, 1, 2])
     
-    def __init__(self, prod1, prod2):
+    #Initialize object state
+    def __init__(self, prod1='Prod1', prod2='Prod2'):
         self.prod1 = prod1
         self.prod2 = prod2
+
+    #Printable representation string
+    def __repr__(self):
+        return f"Product(prod1='{self.prod1}', prod2='{self.prod2}')" 
     
+    #Show first 5 rows of matrix of available products
     def show_matrix(self):
         return self._DF.head()
 
-    def locate_keys(self):
+    #List of available products
+    def list_all_products(self):
         return list(self._DF.index)
     
+    #Locate a product code, group and changeover time
     def locate_product(self):
         return self._DF.xs(self.prod1, axis=0, level=None)[self.prod2].to_dict()
 
