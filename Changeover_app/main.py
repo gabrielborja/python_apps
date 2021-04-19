a05be3c6d2e29885ff68f0091413072df2ac62cd
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-04-19 20:54:26 +0200

    Generate labels and get product method

--- Changeover_app/main.py
+++ Changeover_app/main.py
@@ -12,35 +12,60 @@ class MainApplication(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
         self.master = master
-    
+        self._products = self.list_products()
+        self._prod1 = tk.StringVar(value=self._products[0])
+        self._prod2 = tk.StringVar(value=self._products[1])
+        
     #Create main window object
     def create_window(self):
         self.master.title("Changeover App")
-        self.master.geometry("400x400")
+        #self.master.geometry("470x470")
         self.master.resizable(0,0)
         #master.config(bg="#ffffff")
     
-    #Initialize
+    #Initialize list of available products
     def list_products(self):
         self._my_products = Product()
         return self._my_products.list_all_products()
-        
+       
     #Initialize Widgets
     def create_widgets(self):
-        self._intro_text = tk.Label(self.master, text="Select from the dropdown lists below to convert the changeover " \
-            "time of different products", justify="center", wraplength=250, font=("Monospace", 10))
-        self._combobox_one = ttk.Combobox(self.master, value=self.list_products())
-        self._equal_sign = tk.Label(self.master, text=" = ")
-        self._combobox_two = ttk.Combobox(self.master, value=self.list_products())
-        self._convert_button = tk.Button(self.master, text='Convert')
+        self._intro_text = tk.Label(self.master, text="Select from the dropdown list below to convert from " \
+            "one product to another", justify="center", wraplength=250, font=('Arial', 14))
+        self._combobox_one = ttk.Combobox(self.master, values=self.list_products(), textvariable=self._prod1)
+        self._equal_sign = tk.Label(self.master, text=" = ", font=('Arial', 16))
+        self._combobox_two = ttk.Combobox(self.master, state='readonly', values=self.list_products(), textvariable=self._prod2)
+        self._convert_button = tk.Button(self.master, text='Convert', command=self.get_products, font=('Arial', 12))
+        self._code_label = tk.Label(self.master, text='Code', width=8, font=('Arial', 12))
+        self._group_label = tk.Label(self.master, text='Group', width=8, font=('Arial', 12))
+        self._minutes_label = tk.Label(self.master, text='Minutes', width=8, font=('Arial', 12))
+        self._code_output = tk.Entry(self.master, text='Hola')
+        self._group_output = tk.Entry(self.master, text='Group')
+        self._minutes_output = tk.Entry(self.master, text='Minutes')
+        self._bottom_text = tk.Label(self.master, text='')
     
     #Position Widgets with grid
     def position_widgets(self):
-        self._intro_text.grid(row=0, column=0, columnspan=3, padx=10, pady=20)
-        self._combobox_one.grid(row=1, column=0, padx=10, pady=20, sticky="W")
-        self._equal_sign.grid(row=1, column=1, padx=10, pady=20)
-        self._combobox_two.grid(row=1, column=2, padx=10, pady=20, sticky="E")
-        self._convert_button.grid(row=2, column=0, columnspan=3, padx=10, pady=20, ipadx=50)
+        self._intro_text.grid(row=0, column=0, columnspan=3, padx=15, pady=20)
+        self._combobox_one.grid(row=1, column=0, padx=15, pady=20)
+        self._equal_sign.grid(row=1, column=1, padx=15, pady=20)
+        self._combobox_two.grid(row=1, column=2, padx=15, pady=20)
+        self._convert_button.grid(row=2, column=0, columnspan=3, padx=15, pady=20, ipadx=50)
+        self._code_label.grid(row=3, column=0, padx=5, pady=5)
+        self._group_label.grid(row=3, column=1, padx=5, pady=5)
+        self._minutes_label.grid(row=3, column=2, padx=5, pady=5)
+        self._code_output.grid(row=4, column=0, padx=5, pady=5)
+        self._group_output.grid(row=4, column=1, padx=5, pady=5)
+        self._minutes_output.grid(row=4, column=2, padx=5, pady=5)
+        self._bottom_text.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
+    
+    #Get products from dropdown list
+    def get_products(self):
+        self._prod1 = self._combobox_one.get()
+        self._prod2 = self._combobox_two.get()
+        return(self._prod1, self._prod2)
+
+
         
 
 def main():
@@ -49,6 +74,7 @@ def main():
     app.create_window()
     app.create_widgets()
     app.position_widgets()
+    app.get_products()
     root.mainloop()
 
 if __name__ == "__main__": main()
