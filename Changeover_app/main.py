7733d6c8479c0ca6d3bef0843eeb13a2f494fb5f
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-04-20 20:51:37 +0200

    Link products with convert button and delete output fields

--- Changeover_app/main.py
+++ Changeover_app/main.py
@@ -17,24 +17,22 @@ class MainApplication(tk.Frame):
     #Create main window object
     def create_window(self):
         self.master.title("Changeover App")
-        #self.master.geometry("470x470")
         self.master.resizable(0,0)
-        #master.config(bg="#ffffff")
     
     #Initialize Widgets
     def create_widgets(self):
         self._intro_text = tk.Label(self.master, text="Select from the dropdown list below to convert from " \
             "one product to another", justify="center", wraplength=250, font=('Arial', 14))
-        self._combobox_one = ttk.Combobox(self.master, values=self._list_products, textvariable=self._prod1)
+        self._combobox_one = ttk.Combobox(self.master, values=self._list_products, textvariable=self._prod1, font=('Arial', 12))
         self._equal_sign = tk.Label(self.master, text=" = ", font=('Arial', 16))
-        self._combobox_two = ttk.Combobox(self.master, state='readonly', values=self._list_products, textvariable=self._prod2)
-        self._convert_button = tk.Button(self.master, text='Convert', command=self.get_products, font=('Arial', 12))
+        self._combobox_two = ttk.Combobox(self.master, state='readonly', values=self._list_products, textvariable=self._prod2, font=('Arial', 12))
+        self._convert_button = tk.Button(self.master, text='Convert', command=self.display_products, font=('Arial', 12))
         self._code_label = tk.Label(self.master, text='Code', width=8, font=('Arial', 12))
         self._group_label = tk.Label(self.master, text='Group', width=8, font=('Arial', 12))
         self._minutes_label = tk.Label(self.master, text='Minutes', width=8, font=('Arial', 12))
-        self._code_output = tk.Entry(self.master, text='Hola')
-        self._group_output = tk.Entry(self.master, text='Group')
-        self._minutes_output = tk.Entry(self.master, text='Minutes')
+        self._code_output = tk.Entry(self.master, text='Code', justify='center', font=('Arial', 12))
+        self._group_output = tk.Entry(self.master, text='Group', justify='center', font=('Arial', 12))
+        self._minutes_output = tk.Entry(self.master, text='Minutes', justify='center', font=('Arial', 12))
         self._bottom_text = tk.Label(self.master, text='')
     
     #Position Widgets with grid
@@ -52,20 +50,25 @@ class MainApplication(tk.Frame):
         self._minutes_output.grid(row=4, column=2, padx=5, pady=5)
         self._bottom_text.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
     
+    def _delete_output_entries(self):
+        self._code_output.delete(0, tk.END)
+        self._group_output.delete(0, tk.END)
+        self._minutes_output.delete(0, tk.END)
+
     #Get products from dropdown list
     def get_products(self):
         prod1 = self._combobox_one.get()
         prod2 = self._combobox_two.get()
-        print(prod1, prod2)
         self._product = Product(prod1=prod1, prod2=prod2)
         return self._product.locate_product()
     
     #Display the products to output fields
     def display_products(self):
-        key, val = self.get_products()
-        print(key, val)
-
-
+        self._delete_output_entries()
+        code, group, minutes = self.get_products()
+        self._code_output.insert(0, code)
+        self._group_output.insert(0, group)
+        self._minutes_output.insert(0, minutes)
         
 def main():
     root = tk.Tk()
@@ -73,7 +76,6 @@ def main():
     app.create_window()
     app.create_widgets()
     app.position_widgets()
-    app.display_products()
     root.mainloop()
 
 if __name__ == "__main__": main()
