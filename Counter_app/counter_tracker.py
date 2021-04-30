aea7bfc1f1ba78edd8f33d2e586cb264a37791c3
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-04-30 13:59:14 +0200

    Create button factory

--- Counter_app/counter_tracker.py
+++ Counter_app/counter_tracker.py
@@ -20,38 +20,35 @@ class MainApplication(tk.Frame):
         self.paned_window.add(self.frame2, weight=3)
     
     #Initialize Widgets
-    def create_widgets(self):
-        self.button1 = ttk.Button(self.frame2, text='1', command=lambda: self.callback(1))
-        self.button2 = ttk.Button(self.frame2, text='2', command=lambda: self.callback(2))
-        self.button3 = ttk.Button(self.frame2, text='3', command=lambda: self.callback(3))
-        self.button4 = ttk.Button(self.frame2, text='4', command=lambda: self.callback(4))
-        self.button5 = ttk.Button(self.frame2, text='5', command=lambda: self.callback(5))
-        self.button6 = ttk.Button(self.frame2, text='6', command=lambda: self.callback(6))
-        self.button7 = ttk.Button(self.frame2, text='7', command=lambda: self.callback(7))
-        self.button8 = ttk.Button(self.frame2, text='8', command=lambda: self.callback(8))
-        self.button9 = ttk.Button(self.frame2, text='9', command=lambda: self.callback(9))
-        self.button10 = ttk.Button(self.frame2, text='10', command=lambda: self.callback(10))
-        self.button11 = ttk.Button(self.frame2, text='11', command=lambda: self.callback(11))
-        self.button12 = ttk.Button(self.frame2, text='12', command=lambda: self.callback(12))
-    
-    #Position Widgets with grid
-    def position_widgets(self):
-        self.button1.grid(row=0, column=0, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button2.grid(row=0, column=1, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button3.grid(row=0, column=2, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button4.grid(row=1, column=0, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button5.grid(row=1, column=1, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button6.grid(row=1, column=2, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button7.grid(row=2, column=0, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button8.grid(row=2, column=1, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button9.grid(row=2, column=2, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button10.grid(row=3, column=0, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button11.grid(row=3, column=1, columnspan=1, padx=15, pady=10, ipadx=25)
-        self.button12.grid(row=3, column=2, columnspan=1, padx=15, pady=10, ipadx=25)
+    #def create_widgets(self):
+        #self.button1 = ttk.Button(self.frame2, text='1', command=lambda: self.callback(1))
+        #self.button2 = ttk.Button(self.frame2, text='2', command=lambda: self.callback(2))
+        #self.button3 = ttk.Button(self.frame2, text='3', command=lambda: self.callback(3))
+        #self.button4 = ttk.Button(self.frame2, text='4', command=lambda: self.callback(4))
+        #self.button5 = ttk.Button(self.frame2, text='5', command=lambda: self.callback(5))
+        #self.button6 = ttk.Button(self.frame2, text='6', command=lambda: self.callback(6))
+        #self.button7 = ttk.Button(self.frame2, text='7', command=lambda: self.callback(7))
+        #self.button8 = ttk.Button(self.frame2, text='8', command=lambda: self.callback(8))
+        #self.button9 = ttk.Button(self.frame2, text='9', command=lambda: self.callback(9))
+        #self.button10 = ttk.Button(self.frame2, text='10', command=lambda: self.callback(10))
+        #self.button11 = ttk.Button(self.frame2, text='11', command=lambda: self.callback(11))
+        #self.button12 = ttk.Button(self.frame2, text='12', command=lambda: self.callback(12))
     
     #Create callback fuction
     def callback(self, num):
-        print(num)
+        """Callback factory. Calling it returns function with the number of the button pressed"""
+        def _callback():
+            print(num)
+        return _callback
+
+    #Initialize Widgets
+    def create_widgets(self):
+        names = (str(i+1) for i in range(36)) #Create str names for the buttons
+        self.button = []
+        for i, name in enumerate(names):
+            self.button.append(ttk.Button(self.frame2, text=name, command=self.callback(i+1)))
+            row,col=divmod(i, 3)
+            self.button[i].grid(row=row, column=col, columnspan=1, padx=10, pady=10, ipadx=15)
 
 
 def main():
@@ -59,7 +56,6 @@ def main():
     app = MainApplication(root)
     app.create_frames()
     app.create_widgets()
-    app.position_widgets()
     root.mainloop()
 
 if __name__ == "__main__": main()
