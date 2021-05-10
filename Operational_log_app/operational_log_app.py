a11b6ce68295c89a6dbd16ffc9db17178321cb8e
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-05-10 19:49:10 +0200

    Update Labels and Text grid

--- Operational_log_app/operational_log_app.py
+++ Operational_log_app/operational_log_app.py
@@ -7,6 +7,7 @@ class Feedback:
     def __init__(self, master):
         
         master.title('Operational Log APP')
+        master.geometry('500x615')
         master.resizable(False, False)
         master.configure(background='#383838')
         
@@ -19,39 +20,39 @@ class Feedback:
         self.frame_header = ttk.Frame(master)
         self.frame_header.pack()
         
-        self.logo = tk.PhotoImage(file = 'images/freia_logo.png')
+        self.logo = tk.PhotoImage(file = 'images/freia_logo.png').subsample(2, 2)
         ttk.Label(self.frame_header, image=self.logo).grid(row=0, column=0, rowspan=2)
-        ttk.Label(self.frame_header, text = 'Anlegg Driftslogg', style = 'Header.TLabel').grid(row=0, column=1, padx=15, pady=5)
+        ttk.Label(self.frame_header, text = 'Anlegg Driftslogg', style = 'Header.TLabel').grid(row=0, column=1, padx=15, pady=15)
         ttk.Label(self.frame_header, wraplength = 250,
-                  text = ('Welcome to the program to record information for the Molding Line.\n\n'
-                  'Please write down your comments below.')).grid(row=1, column=1, padx=20, pady=5)
+                  text = ('Velkommen til programmet for å registrere informasjonen fra Anlegget.\n\n'
+                  'Vennligst skriv ned kommentarene nedenfor.')).grid(row=1, column=1, padx=20)
         
         self.frame_content = ttk.Frame(master)
         self.frame_content.pack()
 
-        ttk.Label(self.frame_content, text = 'Shift:').grid(row=0, column=0, padx=5, pady=5, sticky='sw')
-        ttk.Label(self.frame_content, text = 'Machine:').grid(row=0, column=1, padx=5, pady=5, sticky='sw')
-        ttk.Label(self.frame_content, text = 'Comments:').grid(row=2, column=0, padx=5, pady=5, sticky='sw')
+        ttk.Label(self.frame_content, text = 'Skift:').grid(row=0, column=0, pady=5, sticky='sw')
+        ttk.Label(self.frame_content, text = 'Maskin:').grid(row=0, column=1, pady=5, sticky='sw')
+        ttk.Label(self.frame_content, text = 'Kommentarer:').grid(row=2, column=0, pady=5, sticky='sw')
+
+        self.entry_shift = ttk.Entry(self.frame_content, width=27, font=('Arial', 10), justify='left')
+        self.entry_machine = ttk.Entry(self.frame_content, width=27, font=('Arial', 10), justify='left')
+        self.text_comments = tk.Text(self.frame_content, width=60, height=20, font=('Arial', 10))
         
-        self.entry_name = ttk.Entry(self.frame_content, width=24, font=('Arial', 10))
-        self.entry_email = ttk.Entry(self.frame_content, width=24, font=('Arial', 10))
-        self.text_comments = tk.Text(self.frame_content, width=50, height=10, font=('Arial', 10))
-        
-        self.entry_name.grid(row= 1, column=0, padx=5)
-        self.entry_email.grid(row=1, column=1, padx=5)
+        self.entry_shift.grid(row=1, column=0)
+        self.entry_machine.grid(row=1, column=1)
         self.text_comments.grid(row=3, column=0, columnspan=2, padx=5)
-        
+
         ttk.Button(self.frame_content, text = 'Submit', style='TButton',
-                   command = self.submit).grid(row=4, column=0, padx=5, pady=15, sticky='e')
+                   command=self.submit).grid(row=4, column=0, padx=5, pady=15, sticky='e')
         ttk.Button(self.frame_content, text = 'Clear', style='TButton',
-                   command = self.clear).grid(row=4, column=1, padx=5, pady=15, sticky='w')
+                   command=self.clear).grid(row=4, column=1, padx=5, pady=15, sticky='w')
 
     def submit(self):
-        print('Name: {}'.format(self.entry_name.get()))
-        print('Email: {}'.format(self.entry_email.get()))
-        print('Comments: {}'.format(self.text_comments.get(1.0, tk.END)))
+        print(f'Skift: {self.entry_shift.get()}')
+        print(f'Maskin: {self.entry_machine.get()}')
+        print(f'Kommentarer: {self.text_comments.get(1.0, tk.END)}')
         self.clear()
-        messagebox.showinfo(title = 'Explore California Feedback', message = 'Comments Submitted!')
+        messagebox.showinfo(title = 'Driftslogg', message = 'Kommentarer sendt!')
     
     def clear(self):
         self.entry_name.delete(0, tk.END)
