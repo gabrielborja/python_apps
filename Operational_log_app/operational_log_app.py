3f8dbd9c8851ffafbe1a680c03f24d708ec784bd
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-05-13 12:38:09 +0200

    Create date and name entry widgets

--- Operational_log_app/operational_log_app.py
+++ Operational_log_app/operational_log_app.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 class MainApplication:
     def __init__(self, master):
         master.title('Operational Log APP')
-        master.geometry('500x675')
+        master.geometry('500x695+0+0')
         master.resizable(False, False)
         
         self.style = ttk.Style()
@@ -19,38 +19,42 @@ class MainApplication:
         
         self.logo = tk.PhotoImage(file = 'images/freia_logo.png').subsample(2, 2)
         ttk.Label(self.frame_header, image=self.logo).grid(row=0, column=0, rowspan=2)
-        ttk.Label(self.frame_header, text = 'Anlegg Driftslogg', style = 'Header.TLabel').grid(row=0, column=1, padx=15, pady=15)
+        ttk.Label(self.frame_header, text = 'Operational Log', style = 'Header.TLabel').grid(row=0, column=1, padx=15, pady=15)
         ttk.Label(self.frame_header, wraplength = 250,
-                  text = ('Velkommen til programmet for å registrere informasjonen fra Anlegget.\n\n'
-                  'Vennligst skriv kommentarene nedenfor.')).grid(row=1, column=1, padx=20)
+                  text = ('Welcome to the program to record data from the Moulding line.\n\n'
+                  'Please write your comments below.')).grid(row=1, column=1, padx=20, pady=20)
         
         self.frame_content = ttk.Frame(master)
         self.frame_content.pack()
 
-        ttk.Label(self.frame_content, text = 'Shift:').grid(row=0, column=0, pady=5, padx=5, sticky='sw')
-        ttk.Label(self.frame_content, text = 'Machine:').grid(row=0, column=1, pady=5, padx=5, sticky='sw')
-        ttk.Label(self.frame_content, text = 'Time:').grid(row=2, column=0, pady=5, padx=5, sticky='sw')
-        ttk.Label(self.frame_content, text = 'Comments:').grid(row=4, column=0, pady=5, padx=5, sticky='sw')
+        ttk.Label(self.frame_content, text = 'Date:').grid(row=0, column=0, padx=5, pady=5, sticky='w')
+        ttk.Label(self.frame_content, text = 'Name:').grid(row=0, column=1, padx=5, pady=5, sticky='w')
+        ttk.Label(self.frame_content, text = 'Shift:').grid(row=2, column=0, padx=5, pady=5, sticky='w')
+        ttk.Label(self.frame_content, text = 'Machine:').grid(row=2, column=1, padx=5, pady=5, sticky='w')
+        ttk.Label(self.frame_content, text = 'Comments:').grid(row=4, column=0, padx=5, pady=5, sticky='w')
 
-        self.entry_shift = ttk.Combobox(self.frame_content, values=['Day', 'Afternoon', 'Night'], textvariable=['Day'], width=24, font=('Arial', 10), justify='left')
-        self.entry_machine = ttk.Combobox(self.frame_content, values=self.machine_list(), textvariable='Machine_1', width=24, font=('Arial', 10), justify='left')
-        self.entry_hour = ttk.Entry(self.frame_content, width=29, font=('Arial', 10), justify='left')
-        self.text_comments = tk.Text(self.frame_content, width=60, height=20, font=('Arial', 10))
+
+        self.entry_date = ttk.Entry(self.frame_content, width=30, font=('Arial', 10), justify='left')
+        self.entry_name = ttk.Entry(self.frame_content, width=30, font=('Arial', 10), justify='left')
+        self.entry_shift = ttk.Combobox(self.frame_content, values=['Day', 'Afternoon', 'Night'], textvariable=['Day'], width=28, font=('Arial', 10), justify='left')
+        self.entry_machine = ttk.Combobox(self.frame_content, values=self.machine_list(), textvariable='Machine_1', width=28, font=('Arial', 10), justify='left')
+        self.text_comments = tk.Text(self.frame_content, width=64, height=20, font=('Arial', 10))
         
-        self.entry_shift.grid(row=1, column=0)
-        self.entry_machine.grid(row=1, column=1)
-        self.entry_hour.grid(row=3, column=0)
+        self.entry_date.grid(row=1, column=0)
+        self.entry_name.grid(row=1, column=1)
+        self.entry_shift.grid(row=3, column=0)
+        self.entry_machine.grid(row=3, column=1, columnspan=1)
         self.text_comments.grid(row=5, column=0, columnspan=2, padx=5)
 
-        ttk.Button(self.frame_content, text = 'Submit', style='TButton',
-                   command=self.submit).grid(row=6, column=0, padx=5, pady=15, sticky='e')
-        ttk.Button(self.frame_content, text = 'Clear', style='TButton',
-                   command=self.clear).grid(row=6, column=1, padx=5, pady=15, sticky='w')
+        ttk.Button(self.frame_content, text = 'Save Record', style='TButton',
+                   command=self.save_record).grid(row=6, column=0, padx=5, pady=15, ipadx=10, sticky='e')
+        ttk.Button(self.frame_content, text = 'Clear all', style='TButton',
+                   command=self.clear).grid(row=6, column=1, padx=5, pady=15, ipadx=10, sticky='w')
 
     def machine_list(self):
         return [f'Machine_{i}' for i in range(1, 16)]
 
-    def submit(self):
+    def save_record(self):
         print(f'Skift: {self.entry_shift.get()}')
         print(f'Maskin: {self.entry_machine.get()}')
         print(f'Kommentarer: {self.text_comments.get(1.0, tk.END)}')
