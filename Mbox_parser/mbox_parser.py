888b59a9225e1cd382e0644e6a45b2b955c6995a
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-06-08 17:00:03 +0200

    Update save_to_json function and gitignore

--- Mbox_parser/mbox_parser.py
+++ Mbox_parser/mbox_parser.py
@@ -25,10 +25,18 @@ def parse_mbox():
 
 def save_to_json(my_list):
     """Save list to json file"""
-    with open('email_list.json', 'w') as f:
-        json.dump(my_list, f)
+    try:
+        with open('email_list.json', 'w') as f:
+            json.dump(my_list, f)
+    except TypeError:
+        e = 0
+        e += 1
+    print(f'Unserializable obj: {e}')
 
 #Run main program
 if __name__ == "__main__":
+    #email_list = [{'Date': 'Tue, 06 Apr 2021 19:17:22 +0000', 'From': '"Marketing Analytics" <no-reply@t.mail.coursera.org>', 'Subject': 'New Online Course to Help You Develop Effective Digital Marketing Strategies'},
+    #{'Date': 'Tue, 06 Oct 2020 18:26:43 +0000', 'From': '"Humble Bundle" <contact@mailer.humblebundle.com>', 'Subject': '=?utf-8?b?QnJhd2wgZm9yIGl0IGFsbCBpbiB0aGUgTGV04oCZcyBGaWdodCBCdW5kbGUhIPCfpYo=?='},
+    #{'Date': 'Thu, 15 Oct 2020 08:42:51 +0000 (UTC)', 'From': 'Digg Pizza <hei@digg.no>', 'Subject': '=?UTF-8?Q?P=C3=A5minnelse!?= Du har fortsatt tre gratis pizzaer til gode =?UTF-8?B?8J+NlQ==?='}]
     email_list = parse_mbox()
     save_to_json(email_list)
