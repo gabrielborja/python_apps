f4a8187630c5ccc0e0d10ee4e4ef9eaae6206e10
Gabriel Borja <gabrielborjavergara@gmail.com>
2021-08-18 16:51:45 +0200

    Customize the styles of the Ideas Management Dashboard

--- Ideas_dashboard_app/app.py
+++ Ideas_dashboard_app/app.py
@@ -10,7 +10,7 @@ import plotly.express as px
 df = pd.read_csv('ideas_db.csv')
 df = df[df['Trinn']!='1_Pending']
 df['Dato'] = pd.to_datetime(df['Dato'])
-df = df.assign(År = df['Dato'].dt.year,
+df = df.assign(År = df['Dato'].dt.year.astype(str),
                Månder = df['Dato'].dt.month_name())
 df_area = df.groupby(by=['År', 'Månder', 'Linje', 'Område']).agg(Kaizens = ('ID', 'count')).reset_index()
 df_comp = df.groupby(by=['År', 'Linje', 'Trinn']).agg(Status = ('ID', 'count')).reset_index()
@@ -32,12 +32,17 @@ app.layout = html.Div(children=[
     html.H1(children='Ideas Management Dashboard',  className='header-title'),
 
     html.Div(children=[
+        html.H3(children='Year', className='dropdown-title'),
+        html.H3(children='Line', className='dropdown-title')
+    ], className='dropdown-heading'),
+
+    html.Div(children=[
         html.Div(children=[
             dcc.Dropdown(id='Year', options=[{'label': i, 'value': i} for i in df_area['År'].unique()],
-            value='2021', clearable=False)], className='drop-down'),
+            value='2021', clearable=False)], className='dropdown'),
         html.Div(children=[
             dcc.Dropdown(id='Line', options=[{'label': i, 'value': i} for i in df_area['Linje'].unique()],
-            value='J4', clearable=False)], className='drop-down'),
+            value='J4', clearable=False)], className='dropdown'),
     ], className='dropdown-container'),
 
     html.Div(children=[
@@ -56,19 +61,20 @@ app.layout = html.Div(children=[
 
 def update_graphs(selected_year, selected_line):
 
-    df_kaizens = df_area[(df_area['År']==int(selected_year)) & (df_area['Linje']==selected_line)].copy()
-    df_completed = df_comp[(df_comp['År']==int(selected_year)) & (df_comp['Linje']==selected_line)].copy()
+    df_kaizens = df_area[(df_area['År']==selected_year) & (df_area['Linje']==selected_line)].copy()
+    df_completed = df_comp[(df_comp['År']==selected_year) & (df_comp['Linje']==selected_line)].copy()
     
     fig_1 = px.bar(df_kaizens, x='Månder', y='Kaizens', color='Område',
                 title=f'{selected_year} - Generated Ideas in {selected_line}',
                 category_orders={'Månder': ['May', 'June', 'July', 'August']},
                 range_y=[0,25])
     fig_1.add_hline(y=20)
+    fig_1.update_layout(title_x=0.5)
 
     fig_2 = px.bar(df_completed, x='Trinn', y='Status', color='Trinn',
                 title=f'{selected_year} - Completed Ideas in {selected_line}',
                 color_discrete_sequence=['#2A9D8F', '#800080'])
-    fig_2.layout.update(showlegend=False)
+    fig_2.layout.update(showlegend=False, title_x=0.5)
 
     return fig_1, fig_2
 
